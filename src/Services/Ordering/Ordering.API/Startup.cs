7ce125914fc8d52f554ed51a6d642f0885427d0a
PaulBatinic <paul.batinic@gmail.com>
2024-01-15 17:44:59 +0100

    Consume RabbitMQ Event From Ordering Microservice Subscriber of BasketCheckout

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -13,6 +13,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using MassTransit;
+using EventBus.Messages.Common;
+using Ordering.API.EventBusConsumer;
 
 namespace Ordering.API
 {
@@ -30,6 +33,21 @@ namespace Ordering.API
     {
       services.AddApplicationServices();
       services.AddInfrastructureServices(Configuration);
+
+      // MassTransit-RabbitMQ Configuration
+      services.AddMassTransit(config => {
+
+        config.AddConsumer<BasketCheckoutConsumer>();
+
+        config.UsingRabbitMq((ctx, cfg) => {
+          cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+
+          cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c => {
+            c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+          });
+        });
+      });
+      services.AddMassTransitHostedService();
 
       services.AddControllers();
       services.AddSwaggerGen(c =>

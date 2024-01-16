4edb5122b5f98dda653a98783de8d8308918ce61
PaulBatinic <paul.batinic@gmail.com>
2024-01-16 15:22:56 +0100

    Developing Service Implementations for Consuming Api Gateway in AspnetBasics

--- src/WebApps/AspnetRunBasics/Services/CatalogService.cs
+++ src/WebApps/AspnetRunBasics/Services/CatalogService.cs
@@ -1,4 +1,5 @@
-﻿using AspnetRunBasics.Models;
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -9,31 +10,40 @@ namespace AspnetRunBasics.Services
 {
     public class CatalogService : ICatalogService
     {
-        private readonly HttpClient _client;        
+        private readonly HttpClient _client;
 
         public CatalogService(HttpClient client, ILogger<CatalogService> logger)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
         }
 
-        public Task<CatalogModel> CreateCatalog(CatalogModel model)
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
         {
-            throw new NotImplementedException();
+            var response = await _client.GetAsync("/Catalog");
+            return await response.ReadContentAs<List<CatalogModel>>();
         }
 
-        public Task<IEnumerable<CatalogModel>> GetCatalog()
+        public async Task<CatalogModel> GetCatalog(string id)
         {
-            throw new NotImplementedException();
+            var response = await _client.GetAsync($"/Catalog/{id}");
+            return await response.ReadContentAs<CatalogModel>();
         }
 
-        public Task<CatalogModel> GetCatalog(string id)
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
         {
-            throw new NotImplementedException();
+            var response = await _client.GetAsync($"/Catalog/GetProductByCategory/{category}");
+            return await response.ReadContentAs<List<CatalogModel>>();
         }
 
-        public Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        public async Task<CatalogModel> CreateCatalog(CatalogModel model)
         {
-            throw new NotImplementedException();
+            var response = await _client.PostAsJson($"/Catalog", model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<CatalogModel>();
+            else
+            {
+                throw new Exception("Something went wrong when calling api.");
+            }
         }
     }
 }
